418dfba1ad8fe8e1c706388daad67a76b995df8e
NarendraOyc <narendra@oycdev.com>
2015-09-14 18:10:05 +0530

    asd
    asd

--- README.md
+++
@@ -1,2 +0,0 @@
-# ynh
-YNH code
--- application/logs/log-2015-08-07.php
+++
@@ -1,5 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2015-08-07 07:43:58 --> Severity: Core Warning --> PHP Startup: Unable to load dynamic library '/usr/local/lib/php/extensions/no-debug-non-zts-20100525/imagick.so' - libMagickWand.so.2: cannot open shared object file: No such file or directory Unknown 0
-ERROR - 2015-08-07 07:44:40 --> Severity: Core Warning --> PHP Startup: Unable to load dynamic library '/usr/local/lib/php/extensions/no-debug-non-zts-20100525/imagick.so' - libMagickWand.so.2: cannot open shared object file: No such file or directory Unknown 0
-ERROR - 2015-08-07 07:46:15 --> Severity: Core Warning --> PHP Startup: Unable to load dynamic library '/usr/local/lib/php/extensions/no-debug-non-zts-20100525/imagick.so' - libMagickWand.so.2: cannot open shared object file: No such file or directory Unknown 0
